c2dc9820959d9febbf3f7994dc970eb7c3803a73
Michael-Kochis <rhishisikk@yahoo.com>
2026-01-02 14:12:27 -0600

    Javalin basics

--- src/main/java/com/revature/Main.java
+++ src/main/java/com/revature/Main.java
@@ -1,10 +1,11 @@
 package com.revature;
 
-import com.revature.utils.DatabaseUtil;
+import io.javalin.Javalin;
+
+import com.revature.utils.JavelinUtil;
 
 public class Main {
     public static void main(String[] args) {
-        DatabaseUtil.getConnection();
-        System.out.println("no error");
+        Javalin server = JavelinUtil.startServer();
     }
 }
--- src/main/java/com/revature/utils/JavelinUtil.java
+++ src/main/java/com/revature/utils/JavelinUtil.java
@@ -1,7 +1,26 @@
 package com.revature.utils;
 
+import io.javalin.Javalin;
+
 /*
 For the Javalin management
  */
 public class JavelinUtil {
+    public static Javalin server;
+
+    public static Javalin startServer() {
+        server = Javalin.create();
+
+        // Set up the server stack
+        //HealthController healthController = new HealthController();
+
+        //Content Controllers with services and daos.
+
+
+        //Register API endpopints
+        //server.get("/ping", healthController::ping);
+
+
+        return server.start(8080);
+    }
 }
